021401924b46bbdb447d9ce6c538cc62abf7b2ef
vosa183 <vosikal@post.cz>
2026-04-08 15:01:13 +0200

    Update test-pdf.js

--- src/pages/api/test-pdf.js
+++ src/pages/api/test-pdf.js
@@ -11,34 +11,31 @@ export default async function handler(req, res) {
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
     
-    // Zjistíme rozměry stránky
     const { width, height } = firstPage.getSize();
-
     const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
 
     const testName = "JARNÍ CENA JK SOBOTKA";
     const testDate = "Datum konání: 15. 5. 2026";
 
-    // AUTOMATICKÉ CENTROVÁNÍ - změříme šířku konkrétního textu
     const nameWidth = font.widthOfTextAtSize(testName, 24);
-    const dateWidth = font.widthOfTextAtSize(testDate, 16);
+    const dateWidth = font.widthOfTextAtSize(testDate, 18);
 
-    // 1. NÁZEV ZÁVODU (do dřevěné cedule)
+    // 1. NÁZEV ZÁVODU (Konečně do dřevěné cedule!)
     firstPage.drawText(testName, {
-      x: (width - nameWidth) / 2, // Perfektní střed!
-      y: height - 245, // Posunuto výš (menší odečet = vyšší pozice na stránce)
+      x: (width - nameWidth) / 2, 
+      y: height - 275, // Zvětšený odečet = posun dolů do cedule
       size: 24,
       font: font,
-      color: rgb(0.36, 0.25, 0.22), 
+      color: rgb(0.36, 0.25, 0.22), // Westernová hnědá
     });
 
-    // 2. DATUM ZÁVODU (do prázdné stuhy)
+    // 2. DATUM ZÁVODU (Pod stuhu do oblohy)
     firstPage.drawText(testDate, {
       x: (width - dateWidth) / 2, 
-      y: height - 395, // Posunuto níž pod nápis Humprecht
-      size: 16,
+      y: height - 410, // Kousek níž do volného prostoru
+      size: 18, // Zvětšeno z 16 na 18
       font: font,
-      color: rgb(0.2, 0.2, 0.2), 
+      color: rgb(0.36, 0.25, 0.22), // Sjednocená barva
     });
 
     const pdfBytes = await pdfDoc.save();
